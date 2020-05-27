5597fd44efa7ce34a5cadfbe9ee414012bfb937f
goxr3plus <goxr3plus@gmail.com>
2020-05-27 19:25:22 +0300

    Added Fonts

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
 
-export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const fetchFonts = () => {
+   return Font.loadAsync({
+      'open-sans': require('./src/assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./src/assets/fonts/OpenSans-Bold.ttf'),
+   })
 }
+
+const MealDetailsScreen = (props) => {
+   const [fontLoaded, setFontLoaded] = useState(false)
+
+   if (!fontLoaded) {
+      return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+   }
+
+   return (
+      <View style={styles.screen}>
+         <Text style={{ fontFamily: 'open-sans-bold' }}>FUCK YEAH BRO!!!</Text>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+})
+
+export default MealDetailsScreen
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-const CategoriesScreen = (props) => {
+const HomeScreen = (props) => {
    return (
       <View style={styles.screen}>
          <Text></Text>
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
    },
 })
 
-export default CategoriesScreen
+export default HomeScreen
