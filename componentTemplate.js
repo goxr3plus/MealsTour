18c154fc46eb7077e0eff2e631abf46d890ff0ae
goxr3plus <goxr3plus@gmail.com>
2020-05-27 18:52:01 +0300

    First Commit

---
+++ componentTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const template = (props) => {
+   return (
+      <View style={styles.container}>
+         <Text></Text>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+     
+   }
+})
+
+export default template
