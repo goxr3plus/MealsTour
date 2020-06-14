6e6c2e7599d88c90404a6639af040f80dd16c3f5
goxr3plus <goxr3plus@gmail.com>
2020-06-15 00:02:33 +0300

    Filters Screen Content

--- src/screens/FiltersScreen.js
+++ src/screens/FiltersScreen.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, Switch } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from './../components/CustomHeaderButton'
+import Colors from '../utils/constants/Colors'
+
+const FilterSwitch = (props) => (
+   <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch trackColor={{ true: Colors.primaryColor }} thumbColor={'white'} value={props.isGlutenFree} onValueChange={props.onChange}></Switch>
+   </View>
+)
 
 const FiltersScreen = (props) => {
+   const [isGlutenFree, setGlutenFree] = useState(false)
+   const [isLactoseFree, setLactoseFree] = useState(false)
+   const [isVegan, setVigan] = useState(false)
+   const [isVegeterian, setVegeterian] = useState(false)
+
    return (
       <View style={styles.screen}>
          <Text style={styles.title}>Available Filters / Restrictions</Text>
-         <View sttle={styles.filterContainer}></View>
-         <Text>Gluten-free</Text>
-         <Switch></Switch>
+         <FilterSwitch
+            label={'Gluten-free'}
+            isGlutenFree={isGlutenFree}
+            setGlutenFree={setGlutenFree}
+            onChange={(newValue) => setGlutenFree(newValue)}
+         ></FilterSwitch>
+         <FilterSwitch
+            label={'Lactose-free'}
+            isGlutenFree={isLactoseFree}
+            setGlutenFree={setLactoseFree}
+            onChange={(newValue) => setLactoseFree(newValue)}
+         ></FilterSwitch>
+         <FilterSwitch
+            label={'Vegan'}
+            isGlutenFree={isVegan}
+            setGlutenFree={setVigan}
+            onChange={(newValue) => setVigan(newValue)}
+         ></FilterSwitch>
+         <FilterSwitch
+            label={'Vegeterian'}
+            isGlutenFree={isVegeterian}
+            setGlutenFree={setVegeterian}
+            onChange={(newValue) => setVegeterian(newValue)}
+         ></FilterSwitch>
       </View>
    )
 }
@@ -28,7 +62,6 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
    screen: {
       flex: 1,
-      justifyContent: 'center',
       alignItems: 'center',
    },
    title: {
@@ -37,7 +70,13 @@ const styles = StyleSheet.create({
       margin: 20,
       textAlign: 'center',
    },
-   filterContainer: {},
+   filterContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '80%',
+      marginVertical: 20,
+   },
 })
 
 export default FiltersScreen
